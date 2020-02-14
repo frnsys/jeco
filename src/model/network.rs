d7a60aae1bf9fd4cf5ed1882d0a17d1a681c80c8
Francis Tseng <f+accounts@frnsys.com>
2020-02-13 21:00:09 -0500

    set limit to network formation sample size

--- src/model/network.rs
+++ src/model/network.rs
@@ -6,6 +6,8 @@ use rand::rngs::StdRng;
 use rand::seq::SliceRandom;
 use rand::Rng;
 
+static NETWORK_SAMPLE_SIZE: usize = 100;
+
 pub struct Network {
     graph: StableGraph<usize, f32, Directed>,
     agent_to_node: FnvHashMap<usize, NodeIndex>,
@@ -13,8 +15,6 @@ pub struct Network {
 
 impl Network {
     pub fn new(agents: &Vec<Agent>, mut rng: &mut StdRng) -> Network {
-        let sample_size = (0.1 * agents.len() as f32) as usize;
-
         // Network of agents, with trust as weight
         let mut graph = StableGraph::<usize, f32, Directed, u32>::default();
         let mut lookup = FnvHashMap::default();
@@ -27,7 +27,7 @@ impl Network {
         let mut total_edges = 1.;
         for agent in agents.iter() {
             let idx = lookup[&agent.id];
-            let candidates = agents.choose_multiple(&mut rng, sample_size);
+            let candidates = agents.choose_multiple(&mut rng, NETWORK_SAMPLE_SIZE);
             for candidate in candidates {
                 let roll: f32 = rng.gen();
                 let c_idx = lookup[&candidate.id];
