f4c915ce488a62c32322f342c73e14006806aaca
Francis Tseng <f+accounts@frnsys.com>
2020-02-14 22:24:54 -0500

    some clean up

--- src/model/agent.rs
+++ src/model/agent.rs
@@ -6,11 +6,10 @@ use super::network::Network;
 use super::config::SimulationConfig;
 use rand::rngs::StdRng;
 use rand::Rng;
-use rand_distr::StandardNormal;
 use std::cell::{Cell, RefCell};
 use std::fmt::Debug;
 use std::rc::Rc;
-use super::util::{ewma, clamp, gravity};
+use super::util;
 
 pub type Topics = Vector;
 pub type Values = Vector;
@@ -38,16 +37,11 @@ pub struct Agent {
     pub publishers: RefCell<FnvHashMap<PublisherId, f32>>,
 }
 
-static NORMAL_SCALE: f32 = 0.8;
 
 pub fn random_values(rng: &mut StdRng) -> Values {
     // Normal dist, -1 to 1
     let v_vec = (0..VECTOR_SIZE)
-        .map(|_| {
-            let mut val: f32 = rng.sample(StandardNormal);
-            val *= NORMAL_SCALE;
-            clamp(val, -1., 1.)
-        })
+        .map(|_| util::normal_range(rng))
         .collect();
     Values::from_vec(v_vec)
 }
@@ -55,21 +49,14 @@ pub fn random_values(rng: &mut StdRng) -> Values {
 pub fn random_topics(rng: &mut StdRng) -> Topics {
     // Normal dist, 0 to 1
     let i_vec = (0..VECTOR_SIZE)
-        .map(|_| {
-            let mut val = rng.sample(StandardNormal);
-            val = (val + 0.5) * 2.;
-            val *= NORMAL_SCALE;
-            clamp(val, 0., 1.)
-        })
+        .map(|_| util::normal_p(rng))
         .collect();
     Topics::from_vec(i_vec)
 }
 
 impl Agent {
     pub fn new(id: AgentId, mut rng: &mut StdRng) -> Agent {
-        let mut resources = rng.sample(StandardNormal);
-        resources = (resources + 0.5) * 2.;
-        resources = clamp(resources, 0., 1.);
+        let resources = util::normal_p(&mut rng);
 
         Agent {
             id: id,
@@ -86,29 +73,18 @@ impl Agent {
 
     // Return content they create
     pub fn produce(&self, rng: &mut StdRng) -> Option<ContentBody> {
+        // ENH: Take other factors into account for
+        // when an agent produces.
         let p_produce = self.resources;
         let roll: f32 = rng.gen();
 
         if roll < p_produce {
-            let topics = self.interests.map(|v| {
-                let mut val = rng.sample(StandardNormal);
-                val += v;
-                val *= NORMAL_SCALE;
-                clamp(val, 0., 1.)
-            });
+            let topics = self.interests.map(|v| util::normal_p_mu(v, rng));
+            let values = self.values.get().map(|v| util::normal_range_mu(v, rng));
 
-            let values = self.values.get().map(|v| {
-                let mut val = rng.sample(StandardNormal);
-                val += v;
-                val *= NORMAL_SCALE;
-                clamp(val, -1., 1.)
-            });
-
+            // ENH: Take other factors into account
             // Attention cost ranges from 0-100
-            let mut cost = rng.sample(StandardNormal);
-            cost = (cost + 0.5) * 2.;
-            cost = clamp(cost, 0., 1.);
-            cost *= 100.;
+            let cost = util::normal_p(rng) * 100.;
 
             Some(ContentBody {
                 cost: cost,
@@ -158,7 +134,7 @@ impl Agent {
             match c.publisher {
                 Some(p_id) => {
                     let v = publishers.entry(p_id).or_insert(0.5);
-                    *v = ewma(affinity, *v);
+                    *v = util::ewma(affinity, *v);
                 },
                 None => {}
             }
@@ -173,7 +149,7 @@ impl Agent {
                 }
             };
             values.zip_apply(&c.body.values, |v, v_| {
-                v + gravity(v, v_, conf.gravity_stretch, conf.max_influence) * affinity * trust
+                v + util::gravity(v, v_, conf.gravity_stretch, conf.max_influence) * affinity * trust
             });
             self.values.set(values);
 
